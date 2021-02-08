b8d9caf367dcfcb77b9b920679a41f4e39b2b398
Tumi <oluwatumininuojo25@gmail.com>
2021-02-08 18:10:52 +0100

    dart

---
+++ bin/variables_types.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Working with Strings
+  String name;
+  name = 'Tunde';
+  print(name);
+
+  // Working with Numbers
+  int age = 34;
+  double percent = 56.5;
+  print(age);
+  print(percent);
+
+  // Working with Boolean
+  bool isGreater = 4 > 5;
+  print(isGreater);
+}
