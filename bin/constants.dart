4391486061d2d9b5a4f6cf9bb85b77cbf14b1c61
Tumi <oluwatumininuojo25@gmail.com>
2021-02-08 18:25:27 +0100

    constants

--- bin/constants.dart
+++ bin/constants.dart
@@ -3,5 +3,21 @@ void main() {
   final name = 'Peter';
   print(name);
 
-  // Working with cons
+  // Working with constants
+  const PI = 3.142;
+  print(PI);
+
+  // Differences between the two:
+  // A final variable can only be set once and it is initialised when accessed
+  // const variable is implicitly final but it is a compile-time constant
+  // Instance variable can be final but cannot be const though, static const can
+  // be used
+
+}
+
+class Circle {
+
+  final color = 'Red';
+  static const PI = 3.14;
+
 }
