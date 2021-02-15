b078be9c3b81884f54879d382c7df6c83dec5f79
Tumi <oluwatumininuojo25@gmail.com>
2021-02-15 20:09:45 +0100

    conditional_statements

--- bin/conditional_statements.dart
+++ bin/conditional_statements.dart
@@ -1,1 +1,22 @@
-void main() {}
+void main() {
+  int a = 2;
+  int b = 3;
+
+  if (a < b) {
+    print("$a is smaller");
+  } else {
+    print("$b is smaller");
+  }
+
+  a < b ? print("$a is smaller") : print("$b is smaller");
+
+  // OR
+
+  int smallNumber = a < b ? a : b;
+  print(smallNumber);
+
+  String name = null;
+
+  String nameToPrint = name ?? "Guest User";
+  print(nameToPrint);
+}
