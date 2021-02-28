a5a7e6a6ffa699627207d5e01f05d4cf5f04449f
Tumi <oluwatumininuojo25@gmail.com>
2021-02-28 06:54:03 +0100

    exploring "for" loop

--- bin/exploring_for_loop.dart
+++ bin/exploring_for_loop.dart
@@ -4,7 +4,7 @@ void main() {
 
   // Find the even numbers between 1 to 10
 
-  for (int i = 1; i <= 10; i++) {
+  for (var i = 1; i <= 10; i++) {
     if (i % 2 == 0) {
       print(i);
     }
@@ -12,12 +12,11 @@ void main() {
   }
 
   // for.. in loop
-
-  List planetList = [
-    "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Pluto"
+  var planetList = [
+    'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Pluto'
   ];
 
-  for (String planet in planetList) {
+  for (var planet in planetList) {
     print(planet);
   }
 }
