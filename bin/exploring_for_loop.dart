01d14e406e69385c331021cab1465d7ffd78fcfa
Tumi <oluwatumininuojo25@gmail.com>
2021-02-28 06:46:53 +0100

    exploring "for" loop

--- bin/exploring_for_loop.dart
+++ bin/exploring_for_loop.dart
@@ -1,10 +1,13 @@
 void main() {
 
   // For loop
+
+  // Find the even numbers between 1 to 10
+
   for (int i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
 
-    print(i);
   }
-
-  // Find the even nu
 }
